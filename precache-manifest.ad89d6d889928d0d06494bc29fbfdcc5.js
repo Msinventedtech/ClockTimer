9b44f9c0d388b182b7ed0338e0064ecd583fe530
Msinventedtech <93846194+Msinventedtech@users.noreply.github.com>
2022-09-02 23:54:52 +0530

    Updates

---
+++ precache-manifest.ad89d6d889928d0d06494bc29fbfdcc5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "056e6eae51856903599f",
+    "url": "/Msinventedtech/ClockTimer/static/css/main.2b558702.chunk.css"
+  },
+  {
+    "revision": "056e6eae51856903599f",
+    "url": "/Msinventedtech/ClockTimer/static/js/main.056e6eae.chunk.js"
+  },
+  {
+    "revision": "7d8a5b69bced8be18895",
+    "url": "/Msinventedtech/ClockTimer/static/js/1.7d8a5b69.chunk.js"
+  },
+  {
+    "revision": "5f6c0ea9f1db987c729d",
+    "url": "/Msinventedtech/ClockTimer/static/css/2.643f02d8.chunk.css"
+  },
+  {
+    "revision": "5f6c0ea9f1db987c729d",
+    "url": "/Msinventedtech/ClockTimer/static/js/2.5f6c0ea9.chunk.js"
+  },
+  {
+    "revision": "5483c70a9d32ad1cccff",
+    "url": "/Msinventedtech/ClockTimer/static/js/runtime~main.5483c70a.js"
+  },
+  {
+    "revision": "0f45d04c39a42188b71b0b2f7236ab1f",
+    "url": "/Msinventedtech/ClockTimer/index.html"
+  }
+];

730a596297c26ae0dadbf8a92990f73b49b6bb3e
Msinventedtech <93846194+Msinventedtech@users.noreply.github.com>
2022-09-05 00:30:41 +0530

    updated

--- src/Timer.js
+++ src/Timer.js
@@ -1,40 +1,66 @@
 import React from 'react';
 
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { timer: 0, 
+                  paused: true,
+                  seconds: 0,
+                  minutes: 0,
+                  hours: 0
+                };
+	};
+  
+
+  tick = () => {
+  	this.setState({ timer : this.state.timer + 1 });
+    this.setState({hours : Math.floor(this.state.timer / (60 * 60))});
+    
+    let divisor_for_minutes = this.state.timer % (60 * 60);
+    this.setState ({minutes : Math.floor(divisor_for_minutes / 60)});
+
+    let divisor_for_seconds = divisor_for_minutes % 60;
+    this.setState({seconds : Math.ceil(divisor_for_seconds)});
+  }
+  
+	startTimer = () =>{
+		this.interval = setInterval(this.tick,1000);
+    this.setState({ paused : false }); 
+	}
 
 
-class Timer extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = { seconds: 0 };
-    }
-  
-    tick() {
-      this.setState(state => ({
-        seconds: state.seconds + 1
-      }));
-    }
-  
-    componentDidMount() {
-      this.interval = setInterval(() => this.tick(), 1000);
-    }
-  
-    componentWillUnmount() {
-      clearInterval(this.interval);
-    }
+  stopTimer = () => {
+  	clearInterval( this.interval );
+    this.setState({ paused : true });
+	}
 
+  
+  reset = () => {
+  	this.setState({ timer : 0, paused: true });
+    clearInterval( this.interval );
+    this.setState({seconds: 0, minutes: 0, hours: 0})
+  }
+  
+  onClickPause = () => {
+    if(this.state.paused){
+      this.startTimer();
+    }
+    else{
+      this.stopTimer();
+    }
+  }
    
     render() {
       return ( 
       <div>
       <h1 className='tc f1'> TIMER CLOCK</h1>
-    
       <div className='tc'>
               <section class="tc bg-light-green br3 pa5 ma2 dib bw2">
-              <h1 class="f-6 mb1">00</h1>
+              <h1 class="f-6 mb1">{this.state.hours}</h1>
               <h2 class="f5 fw4 gray mt0">Hour</h2>
               </section>
               <section class="tc bg-light-green br3 pa5 ma2 dib bw2">
-              <h1 class="f-6 mb1">00</h1>
+              <h1 class="f-6 mb1">{this.state.minutes}</h1>
               <h2 class="f5 fw4 gray mt0">MINUTE</h2>
               </section>
               <section class="tc bg-light-green br3 pa5 ma2 dib bw2">
@@ -44,16 +70,20 @@ class Timer extends React.Component {
       </div>
           <div className='flex justify-center'>
                 <div className='tc pa4 mr2'>
-                <button className='br4 f2 link dim ph3 grow dib pv2 mb2 dib white bg-dark-blue' type="button">START</button>
+                <button onClick={this.startTimer} 
+                className='br4 f2 link dim ph3 grow dib pv2 mb2 dib white bg-dark-blue' type="button">START</button>
                 </div>
                 <div className='tc pa4 mr2'>
-                <button className='br4 f2 link dim ph3 grow dib pv2 mb2 dib white bg-dark-blue' type="button">STOP</button>
+                <button onClickCapture={this.stopTimer}
+                className='br4 f2 link dim ph3 grow dib pv2 mb2 dib white bg-dark-blue' type="button">STOP</button>
                 </div>
                 <div  className='tc pa4 mr2'>
-                <button className='br4 f2 link dim ph3 grow dib pv2 mb2 dib white bg-dark-blue' type="button">PAUSE</button>
+                <button onClick={this.onClickPause}
+                className ='br4 f2 link dim ph3 grow dib pv2 mb2 dib white bg-dark-blue' type="button">PAUSE</button>
                 </div>
                 <div className='tc pa4 mr2'>
-                <button className='br4 f2 link dim ph3 grow dib pv2 mb2 dib white bg-dark-blue' type="button">RESET</button>
+                <button onClick={this.reset}
+                className='br4 f2 link dim ph3 grow dib pv2 mb2 dib white bg-dark-blue' type="button">RESET</button>
                 </div>
           </div>
           </div>
@@ -62,4 +92,4 @@ class Timer extends React.Component {
   }
 
 
-  export default Timer;
+  export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Timer from './Timer';
+import App from './Timer';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Timer />
+    <App />
   </React.StrictMode>
 );
 
